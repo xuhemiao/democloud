f8a07c867966733c976f89eb140b52a270454b54
xuhemiao <1299526240@qq.com>
2021-04-28 16:54:39 +0800

    know 修改

--- cloudclient/src/main/java/com/xu/CloudclientApplication.java
+++ cloudclient/src/main/java/com/xu/CloudclientApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +14,12 @@ public class CloudclientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudclientApplication.class, args);
+    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
     }
 
 }
