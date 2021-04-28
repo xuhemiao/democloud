37589d7b0d58849314870f7168bcd40e5048ae7d
xuhemiao <1299526240@qq.com>
2021-04-28 09:57:23 +0800

    é服务化和高可用

--- cloudserver/src/main/java/com/xu/CloudserverApplication.java
+++ cloudserver/src/main/java/com/xu/CloudserverApplication.java
@@ -2,10 +2,12 @@ package com.xu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudserverApplication {
 
     public static void main(String[] args) {
